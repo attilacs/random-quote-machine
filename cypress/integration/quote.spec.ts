23402676e644f359d4014bae37c5a5486bfab9ae
Attila Csanyi <csanyiattila23@gmail.com>
2021-12-29 10:20:23 +0100

    Create test for text element

--- cypress/integration/quote.spec.ts
+++ cypress/integration/quote.spec.ts
@@ -7,4 +7,10 @@ describe("Display quotes", () => {
     cy.get("div[data-test=quote-box]");
     cy.get("#quote-box");
   });
+
+  it('should contain an element with id="text" within #quote-box', () => {
+    cy.get("div[data-test=quote-box]");
+    cy.get("div[data-test=text]");
+    cy.get("#text");
+  });
 });
