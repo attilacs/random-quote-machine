04feefb86299299ef785c0a47b54c82403fb5cb8
Attila Csanyi <csanyiattila23@gmail.com>
2021-12-29 10:46:58 +0100

    Create style for buttons

---
+++ src/styles/Button.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.a`
+  align-items: center;
+  border-radius: 3px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  text-decoration: none;
+  user-select: none;
+`;
