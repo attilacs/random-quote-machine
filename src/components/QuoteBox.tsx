643683b8a0a27ca0d805002d3e7e34066c7d12ed
Attila Csanyi <csanyiattila23@gmail.com>
2021-12-29 10:20:42 +0100

    Create text element

--- src/components/QuoteBox.tsx
+++ src/components/QuoteBox.tsx
@@ -8,8 +8,18 @@ const QuoteContainer = styled.div`
   width: 450px;
 `;
 
+const Text = styled.div`
+  font-size: 28px;
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
 const QuoteBox = () => {
-  return <QuoteContainer id="quote-box" data-test="quote-box"></QuoteContainer>;
+  return (
+    <QuoteContainer id="quote-box" data-test="quote-box">
+      <Text id="text" data-test="text"></Text>
+    </QuoteContainer>
+  );
 };
 
 export default QuoteBox;
