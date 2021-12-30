d23c215c4db357fb6472e452a69b1d506212b629
Attila Csanyi <csanyiattila23@gmail.com>
2021-12-30 11:38:45 +0100

    Create test for download error

--- cypress/integration/quote.spec.ts
+++ cypress/integration/quote.spec.ts
@@ -24,4 +24,15 @@ describe("Display quotes", () => {
     cy.contains("Loading...");
 });
 
+  it("should show error message on failed dowload attempt", () => {
+    cy.intercept(
+      "GET",
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+      {
+        statusCode: 404
+      }
+    );
+    cy.wait(3500);
+    cy.contains("An error occurred...");
+});
 });
