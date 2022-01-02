f5c0e65d4a8790dd39cdb50007e76676622dae0a
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-02 15:31:56 +0100

    Change quote page flex direction to column

--- src/styles/QuotePage.tsx
+++ src/styles/QuotePage.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const QuotePageStyled = styled.div`
   align-items: center;
   display: flex;
+  flex-direction: column;
   height: 100vh;
   justify-content: center;
 `;
