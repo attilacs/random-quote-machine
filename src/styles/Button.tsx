071dce80a00f771646176bf33119b37fb2f9778c
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-02 15:33:17 +0100

    Set button text color

--- src/styles/Button.tsx
+++ src/styles/Button.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const ButtonStyled = styled.a`
   align-items: center;
   border-radius: 3px;
+  color: #fff;
   cursor: pointer;
   display: flex;
   justify-content: center;
